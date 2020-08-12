b9c0ccd9a71ca7b09bb217c70b64e1a8764f3989
jcelsocosta <jccl@cin.ufpe.br>
2020-08-12 18:14:25 -0300

    sample project

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule} from '@angular/router';
+import {ReactiveFormsModule} from '@angular/forms';
+import {TopBarComponent} from './top-bar/top-bar.component';
+import {ProductListComponent} from './product-list/product-list.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {path: '', component: ProductListComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
