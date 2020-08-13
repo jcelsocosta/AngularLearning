84057191a144432233046de82f54d2c5fb65e20a
jcelsocosta <jccl@cin.ufpe.br>
2020-08-13 17:09:34 -0300

    Manage Data

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -7,6 +7,9 @@ import {ProductListComponent} from './product-list/product-list.component';
 import { AppComponent } from './app.component';
 import {ProductAlertsComponent} from './product-alert/product-alerts.component';
 import {ProductDetailsComponent} from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { HttpClientModule} from '@angular/common/http';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   declarations: [
@@ -14,15 +17,20 @@ import {ProductDetailsComponent} from './product-details/product-details.compone
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent
     
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       {path: '', component: ProductListComponent},
       {path: 'products/:productId',component: ProductDetailsComponent},
+      {path:'cart', component: CartComponent},
+      {path: 'shipping', component: ShippingComponent}
     ])
   ],
   providers: [],
